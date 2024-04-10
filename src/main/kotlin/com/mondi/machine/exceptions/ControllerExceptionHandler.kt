650df20b50f582379f2ac6e23518dd6fe8bf6488
fsngpz <fsangap18@gmail.com>
2024-04-10 14:16:38 +0700

    add controller exception handler

---
+++ src/main/kotlin/com/mondi/machine/exceptions/ControllerExceptionHandler.kt
@@ -0,0 +1,30 @@
+package com.mondi.machine.exceptions
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+
+/**
+ * The class for controller exception handler.
+ *
+ * @author Ferdinand Sangap
+ * @since 2024-04-10
+ */
+@ControllerAdvice
+class ControllerExceptionHandler {
+
+  /**
+   * Handle general exception.
+   *
+   * @param e the [Exception].
+   * @return the [ErrorResponse] with [HttpStatus.INTERNAL_SERVER_ERROR].
+   */
+  @ExceptionHandler
+  fun handleGeneralException(e: Exception): ResponseEntity<ErrorResponse> {
+    // -- setup the instance of error response --
+    val errorResponse = ErrorResponse(e.message ?: e.stackTraceToString())
+    // -- return as response entity --
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse)
+  }
+}
---
+++ src/main/kotlin/com/mondi/machine/exceptions/ErrorResponse.kt
@@ -0,0 +1,9 @@
+package com.mondi.machine.exceptions
+
+/**
+ * The model class of response error.
+ *
+ * @author Ferdinand Sangap
+ * @since 2024-04-10
+ */
+data class ErrorResponse(val message: String)
