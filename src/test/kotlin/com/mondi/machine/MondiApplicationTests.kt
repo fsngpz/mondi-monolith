6c0202cba3f9fe7b98808a92255fe1944f2c3c25
fsngpz <91114367+fsngpz@users.noreply.github.com>
2024-08-03 23:24:03 +0700

    expand test (#1)
    * expand test
    
    * move the action files
    
    * fix tests

--- src/test/kotlin/com/mondi/machine/MondiApplicationTests.kt
+++ src/test/kotlin/com/mondi/machine/MondiApplicationTests.kt
@@ -2,11 +2,13 @@ package com.mondi.machine
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MondiApplicationTests {
 
-  @Test
-  fun contextLoads() {
-  }
+    @Test
+    fun contextLoads() {
+    }
 }
